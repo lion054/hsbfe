1fc49a3e4819ddd88e5d6860a6e763dc28ec7e0b
Lionel Siduna <lionel.siduna@gmail.com>
2022-06-30 02:36:14 +0200

    Add files via upload

--- src/components/layouts/WalletConnect.jsx
+++ src/components/layouts/WalletConnect.jsx
@@ -1,14 +1,8 @@
 import React , { useState } from 'react';
 import { Link } from 'react-router-dom'
 import img1 from '../../assets/images/icon/icon-1.svg'
-import img2 from '../../assets/images/icon/icon-2.svg'
 import img3 from '../../assets/images/icon/icon-3.svg'
-import img4 from '../../assets/images/icon/icon-4.svg'
 import img5 from '../../assets/images/icon/icon-5.svg'
-import img6 from '../../assets/images/icon/icon-6.svg'
-import img7 from '../../assets/images/icon/icon-7.png'
-import img8 from '../../assets/images/icon/icon-8.svg'
-import img9 from '../../assets/images/icon/icon-9.svg'
 
 const WalletConnect = () => {
     const [data] = useState(
